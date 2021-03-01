aff32649767f6afb8c9419d1d84fbda2ccfdbf78
valentingenev <valentin_genev@abv.bg>
2021-03-01 23:16:17 +0200

    [=] please work

--- .github/.ops/provision-local.sh
+++ .github/.ops/provision-local.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-pwd
-
 # apt-get -y update
 # apt-get install -y --allow-downgrades --allow-remove-essential --allow-change-held-packages curl software-properties-common mc gcc g++ make
 
